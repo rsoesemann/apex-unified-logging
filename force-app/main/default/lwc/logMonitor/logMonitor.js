bcff832cd2b80e6e4eaed21a319cd134790405cc
rsosemann <robert@soesemann.com>
2021-07-13 17:07:00 +0200

    Added permset, Replace Aura with LWC (partial)

---
+++ force-app/main/default/lwc/logMonitor/logMonitor.js
@@ -0,0 +1,159 @@
+import { LightningElement } from "lwc";
+import { subscribe, unsubscribe, onError } from "lightning/empApi";
+import userId from "@salesforce/user/Id";
+import longTimeFormat from "@salesforce/i18n/dateTime.longTimeFormat";
+
+export default class LogMonitor extends LightningElement {
+
+    subscription = {};
+    isMuted = false;
+    logs = {};
+    columns = [];
+    treeData = [];
+
+
+    connectedCallback() {
+        this.initGrid();
+        this.registerErrorListener();
+        this.subscribe();
+    }
+
+
+    disconnectedCallback() {
+        this.unsubscribe();
+    }
+
+
+    initGrid() {
+        this.columns = [
+            {
+                type: "number",
+                fieldName: "context",
+                label: "Context",
+                initialWidth: 100
+            },
+            {
+                type: "text",
+                fieldName: "time",
+                label: "Time",
+                initialWidth: 100
+            },
+            {
+                type: "text",
+                fieldName: "txt_Class__c",
+                label: "Class",
+                initialWidth: 200
+            },
+            {
+                type: "text",
+                fieldName: "txt_Method__c",
+                label: "Method"
+            },
+            {
+                type: "text",
+                fieldName: "txl_Message__c",
+                label: "Message",
+                initialWidth: 200
+            },
+        ];
+    }
+
+
+    subscribe() {
+        const callback = function(message) {
+            this.receive(message);
+        };
+
+        subscribe("/event/Log__e", -1, callback).then(response => {
+            this.subscription = response;
+        });
+    }
+
+
+    unsubscribe() {
+        unsubscribe(this.subscription, response => {
+            this.subscription = null;
+        });
+    }
+
+
+    registerErrorListener() {
+        onError(error => {
+            this.dispatchEvent( new ShowToastEvent({
+                variant: "error",
+                title: "Received error from server:",
+                message: error,
+            }) );
+        });
+    }
+
+
+    receive(message) {
+        const log = message.data.payload;
+
+        if(log.txt_User__c === userId) {
+            log.time = new Intl.DateTimeFormat(longTimeFormat).format(log.CreatedDate);
+            this.store(log);
+
+            this.renderTree();
+        }
+    }
+
+
+    store(log) {
+        const context = log.txt_Context__c;
+
+        this.logs[context] = this.logs[context] || [];
+        this.logs[context].push(log);
+    }
+
+
+    reset() {
+        this.logs = {};
+        this.treeData = [];
+    }
+  
+
+    toggleMute() {
+        this.isMuted = !this.isMuted;
+
+        if(this.isMuted) {
+            this.unsubscribe();
+        }
+        else {
+            this.subscribe();
+        }
+    }
+
+
+    get muteIcon() {
+        return (this.isMuted) ? "utility:volume_off" : "utility:volume_high";
+    }
+
+    
+    get muteLabel() {
+        return (this.isMuted) ? "Unmute" : "Mute";
+    }
+
+
+    renderTree() {
+      let index = 1;
+      this.treeData = [];
+
+      for(const context in this.logs) {
+            let log = this.logs[context][0];
+            log.context = index;
+
+            if(this.logs[context].length > 1) {
+                log._children = logs[context].slice(1);
+            }
+
+            this.treeData.push(log);
+            index++;
+          }
+
+        this.template
+                .querySelector("lightning-tree-grid")
+                .expandAll();
+    }
+}
